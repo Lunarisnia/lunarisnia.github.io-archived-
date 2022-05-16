e7809afd5a158989966ccf99b3028f8f702ca7d4
Lunarisnia <r.arswendo.r@gmail.com>
2022-05-16 19:35:25 +0700

    firebase connected

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <KillCount/>
+      <KillCount :count="count"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import KillCount from './components/KillCount.vue';
+import { getDatabase, ref, onValue } from "firebase/database";
+import app from './config/firebase';
 
 export default {
   name: 'App',
@@ -15,9 +17,16 @@ export default {
   components: {
     KillCount,
   },
+  async beforeMount() {
+    const database = getDatabase(app);
+    const killCountRef = ref(database, '/killCount');
+    onValue(killCountRef, (snapshot) => {
+      this.count = snapshot.val();
+    });
+  },
 
   data: () => ({
-    //
+    count: 0,
   }),
 };
 </script>
--- src/components/KillCount.vue
+++ src/components/KillCount.vue
@@ -2,19 +2,24 @@
   <v-container fill-height fluid class="bg tint">
     <v-row align="center" justify="center" class="text-center">
       <v-col>
-        <h1 class="mainKillCount" transition="fade-transition">99</h1>
+        <h1 class="mainKillCount" transition="fade-transition">{{count}}</h1>
         <h1 class="subtitle">Fish Casualities in</h1>
         <img
           src="https://dkli3tbfz4zj3.cloudfront.net/all/201901XX_SN_BZ_Buy/20190127_SN_BZ_Logo_Horizontal_800.png"
           class="gameTitle"
         />
+        <h1 class="passiveAggressiveInsult">and still going strong</h1>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    count: Number,
+  }
+};
 </script>
 
 <style scoped>
@@ -36,6 +41,16 @@ export default {};
   -o-animation: fadein 2s; /* Opera < 12.1 */
   animation: fadein 2s;
 }
+.passiveAggressiveInsult {
+  font-size: 60%;
+  color: white;
+  opacity: 10%;
+  -webkit-animation: fadeinTrans 15s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadeinTrans 15s; /* Firefox < 16 */
+  -ms-animation: fadeinTrans 15s; /* Internet Explorer */
+  -o-animation: fadeinTrans 15s; /* Opera < 12.1 */
+  animation: fadeinTrans 15s;
+}
 .gameTitle {
   width: 10%;
   -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
@@ -54,6 +69,16 @@ export default {};
   -ms-animation: fadein 2s; /* Internet Explorer */
   -o-animation: fadein 2s; /* Opera < 12.1 */
   animation: fadein 2s;
+}
+
+
+@keyframes fadeinTrans {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 0.1;
+  }
 }
 
 @keyframes fadein {
