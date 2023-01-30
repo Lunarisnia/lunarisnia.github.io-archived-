2d4ea2622246fae4d0402c03ec50298c928b2814
Lunarisnia <r.arswendo.r@gmail.com>
2023-01-30 23:21:53 +0700

    added more progress

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
@@ -30,7 +31,7 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({id, date, title}) => {
             return (
               <li className={utilStyles.listItem} key={id}>
-                {title}
+                <Link href={`/posts/${id}`}>{title}</Link>
                 <br />
                 {id}
                 <br />
